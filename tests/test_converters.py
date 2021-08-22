5d3f84f9e37ec07087ae1fd49447ef8e1f3f79b0
Alexander Gude <alex.public.account@gmail.com>
2021-08-22 15:01:59 -0700

    Add test for `cellphone_use_to_bool`

--- tests/test_converters.py
+++ tests/test_converters.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from switrs_to_sqlite.converters import convert, negative, string_to_bool, county_city_location_to_county
+from switrs_to_sqlite.converters import convert, negative, string_to_bool, county_city_location_to_county, cellphone_use_to_bool
 
 
 # Test the convert function
@@ -99,6 +99,7 @@ def test_nulls():
     for val, nulls in nones:
         assert string_to_bool(val=val, nulls=nulls) is None
 
+
 def test_county_city_location_to_county():
     convert_vals = (
         ("0145", str, None, "01"),
@@ -115,3 +116,25 @@ def test_county_city_location_to_county():
     )
     for val, dtype, nulls, res in convert_vals:
         assert county_city_location_to_county(val=val, dtype=dtype, nulls=nulls) == res
+
+
+def test_cellphone_use_to_bool():
+    convert_vals = (
+        ("B", int, None, True),
+        ("C", int, None, False),
+        ("D", int, None, None),
+        ("1", int, None, True),
+        ("2", int, None, True),
+        ("3", int, None, False),
+        # Nulls that return None
+        ("9", int, ["9"], None),
+        ("a", str, ["a"], None),
+        ("1.", float, ["1."], None),
+        ("9", None, ["9"], None),
+        ("a", None, ["a"], None),
+        ("1.", None, ["1."], None),
+        # Conversion failure
+        ("a", int, None, None),
+    )
+    for val, dtype, nulls, res in convert_vals:
+        assert cellphone_use_to_bool(val=val, dtype=dtype, nulls=nulls) == res
