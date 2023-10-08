9ebc69f5e40861142388ff19ce527bc25affd9cb
Alexander Gude <alex.public.account@gmail.com>
2023-10-08 14:26:39 -0700

    Fix pypandoc error and bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ version = re.search(
 try:
     import pypandoc
 
-    long_description = pypandoc.convert("README.md", "rst")
+    long_description = pypandoc.convert_file("README.md", "rst")
 except ImportError:
     long_description = ""
 
